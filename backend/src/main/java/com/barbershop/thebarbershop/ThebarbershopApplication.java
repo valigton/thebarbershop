e438f1694189442cdf0a27ff00911119f1c39f81
valigton <valigtonvsantos@gmail.com>
2024-03-02 16:42:02 -0300

    Iniciando backend da aplicacao

---
+++ backend/src/main/java/com/barbershop/thebarbershop/ThebarbershopApplication.java
@@ -0,0 +1,16 @@
+package com.barbershop.thebarbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebarbershopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ThebarbershopApplication.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ backend/src/test/java/com/barbershop/thebarbershop/ThebarbershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbershop.thebarbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebarbershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
